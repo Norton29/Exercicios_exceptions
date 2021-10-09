caad7b1ad2ea8fbb36eb9ddbf04a47d6f0e1b8df
Norton29 <norton290194@gmail.com>
2021-10-09 19:06:21 -0300

    Estrutura

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -3,6 +3,8 @@ package Application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.entities.Account;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -11,8 +13,21 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		
 		
+		System.out.println("Enter account data");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Cliente: ");
+		String cliente = sc.next();
+		System.out.print("Saldo Inicial: ");
+		Double saldo = sc.nextDouble();
+		System.out.print("Limite de Saque: ");
+		Double limiteDeSaque = sc.nextDouble();
 		
+		Account account = new Account(number, cliente, saldo, limiteDeSaque);
 		
+		System.out.print("Entre com o valor de saque: ");
+		Double saque = sc.nextDouble();
+		System.out.print("Saldo atualizado: " + (saldo - saque) );
 		
 		
 		sc.close();
